f35e633a6c1c5579a344262dcf9e7238209d60a9
Talim webdeveloper <talim4729@gmail.com>
2026-03-11 17:12:07 +0530

    fix github pages deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {  Route, Routes } from 'react-router-dom';
 import './App.css'
 import Layout from './pages/Layout';
 import Home from './pages/Home';
